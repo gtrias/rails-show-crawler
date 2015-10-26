e9393e736f370269c7e93c5fafc17de778c46b30
Genar Trias Ortiz <genar@acs.li>
2015-10-26 10:25:40 +0100

    crawler and link processing decoupled

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -15,53 +15,8 @@ class ShowsController < ApplicationController
   # GET /crawl/1
   # GET /crawl/1.json
   def crawl
-    require 'nokogiri'
-    require 'open-uri'
-
-    url_base = "http://www.mejortorrent.com"
-
-    doc = Nokogiri::HTML(open(url_base + "/secciones.php?sec=buscador&valor=" + @show.name))
-    doc.css('a').each do |link|
-        # Rails.logger.debug("Show: #{link.inspect}")
-        # Rails.logger.debug("Show text: #{link['text']}")
-
-        if /(\d)-Temporada/.match(link['href'])
-            # Rails.logger.debug("Show href: #{link['href']}")
-            torrentsdoc = Nokogiri::HTML(open(url_base + link['href']))
-            # season = $1
-            torrentsdoc.css('a').each do |tlink|
-                # Rails.logger.debug("Show href: #{tlink['href']}")
-                # Rails.logger.debug("Show season: #{$1}")
-                # Show href: /serie-episodio-descargar-torrent-25036-The-Following-3x03.html
-                if /([0-9])x([0-9]{2})/.match(tlink['href'])
-                    season = $1
-                    @season = season_control(@show, season)
-                    episode = $2
-
-
-                    unless Chapter.where(number: episode, season_id: season).first
-                        chapter_control(@show, season, episode)
-                        # Check here if we have this chapter or not
-                        # Rails.logger.debug("matched show link: #{tlink['href']}")
-                        episodepage = Nokogiri::HTML(open(url_base + tlink['href']))
-                        episodepage.css('a').each do |stlink|
-                            if /secciones\.php\?sec=descargas&ap=contar&tabla=series/.match(stlink['href'])
-                                episodetorrentpage = Nokogiri::HTML(open(url_base + '/' + stlink['href']))
-                                episodetorrentpage.css('a').each do |stplink|
-                                    if /\.torrent$/.match(stplink['href'])
-                                        Rails.logger.debug("matched show: #{stplink['href']}")
-                                        url = url_base + stplink['href']
-                                        add_torrent(url, show, season, episode)
-                                        @link = Link.new(url: url, show: show, season: season, chapter: episode)
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
+    collected_links = mejortorrent_crawl
+    process_links(collected_links)
   end
 
   # GET /shows/new
@@ -157,9 +112,68 @@ class ShowsController < ApplicationController
         torrent.set_location location , true
         Rails.logger.debug("--------> Location: #{location}")
         torrent.save!
-
       rescue => ex
         logger.error ex.message
+      end
+    end
+
+    def mejortorrent_crawl
+      require 'nokogiri'
+      require 'open-uri'
+      collected_links = Array.new
+
+      url_base = "http://www.mejortorrent.com"
+
+      doc = Nokogiri::HTML(open(url_base + "/secciones.php?sec=buscador&valor=" + @show.name))
+      doc.css('a').each do |link|
+          # Rails.logger.debug("Show: #{link.inspect}")
+          # Rails.logger.debug("Show text: #{link['text']}")
+
+          if /(\d)-Temporada/.match(link['href'])
+              # Rails.logger.debug("Show href: #{link['href']}")
+              torrentsdoc = Nokogiri::HTML(open(url_base + link['href']))
+              # season = $1
+              torrentsdoc.css('a').each do |tlink|
+                  # Rails.logger.debug("Show href: #{tlink['href']}")
+                  # Rails.logger.debug("Show season: #{$1}")
+                  # Show href: /serie-episodio-descargar-torrent-25036-The-Following-3x03.html
+                  if /([0-9])x([0-9]{2})/.match(tlink['href'])
+                      season = $1
+                      @season = season_control(@show, season)
+                      episode = $2
+
+
+                      # Check here if we have this chapter or not
+                      # Rails.logger.debug("matched show link: #{tlink['href']}")
+                      episodepage = Nokogiri::HTML(open(url_base + tlink['href']))
+                      episodepage.css('a').each do |stlink|
+                        if /secciones\.php\?sec=descargas&ap=contar&tabla=series/.match(stlink['href'])
+                          episodetorrentpage = Nokogiri::HTML(open(url_base + '/' + stlink['href']))
+                          episodetorrentpage.css('a').each do |stplink|
+                            if /\.torrent$/.match(stplink['href'])
+                              Rails.logger.debug("matched show: #{stplink['href']}")
+                              url = url_base + stplink['href']
+                              @link = Link.new(url: url, show: show, season: season, chapter: episode)
+                              collected_links << @link
+                            end
+                          end
+                        end
+                      end
+                  end
+              end
+          end
+      end
+
+      return collected_links
+
+    end
+
+    def process_links(link_collection)
+      link_collection.each do |link|
+        unless Chapter.where(number: link.chapter, season_id: link.season).first
+          chapter_control(@show, link.season, link.chapter)
+          add_torrent(link.url, link.show, link.season, link.chapter)
+        end
       end
     end
 end
