b6e734c9ac777663fd29254ffd84e776000870e3
Genar Trias Ortiz <genar@acs.li>
2015-10-28 09:22:29 +0100

    some minor model testing

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,6 @@
 class Show < ActiveRecord::Base
     has_and_belongs_to_many :qualities
     has_many :seasons, dependent: :destroy
+
+    validates :name, presence: true
 end
--- test/models/show_test.rb
+++ test/models/show_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ShowTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save tvshow without name" do
+    show = Show.new
+    assert_not show.save, "Saved the show without a name"
+  end
 end
