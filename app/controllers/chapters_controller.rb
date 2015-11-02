3234052ec49b334d58b43589d7e1740d5ae12e62
Genar Trias Ortiz <genar@acs.li>
2015-11-02 09:51:25 +0100

    added chapter removing functionality

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,2 +1,19 @@
 class ChaptersController < ApplicationController
+  before_action :set_chapter, only: [:destroy]
+
+  # DELETE /chapters/1
+  # DELETE /chapters/1.json
+  def destroy
+    @chapter.destroy
+    respond_to do |format|
+      format.html { redirect_to shows_url, notice: 'Chapter was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_chapter
+      @chapter = Chapter.find(params[:id])
+    end
 end
